5c47c728ef6b1040656ae161e24dd8efeb23873e
Que Battle <lbattlepro@gmail.com>
2019-02-21 21:16:41 -0600

    Added Event to Naviagation

--- index.js
+++ index.js
@@ -4,15 +4,66 @@ import Header from './src/Header';
 import Content from './src/Content';
 // import nameChecker from './src/Greeter';
 
+var home = {
+    'title': 'Welcome to my Savvys Coders website!'
+};
+
+
+var blog = {
+    'title': 'Welcome to my blog!'
+};
+
+
+var contact = {
+    'title': 'Welcome to my contact!'
+};
+
+
+var projects = {
+    'title': 'Welcome to my projects!'
+};
+
 
 var root = document.querySelector('#root');
 
-
-root.innerHTML = `
-${Navigation()}
-${Footer()}
-${Header('"What does Que represent?"')}
-${Content()}
+function render(state){
+    root.innerHTML = `
+${Navigation(state)}
+${Footer(state)}
+${Header(state.title)}
+${Content(state)}
 `;
+}
+
+render(home);
+
+
+document.querySelector('#nav li:nth-child(1) > a').addEventListener('click', (e) => {
+    console.log(e.preventDefault());
+    console.log(e.target.href);
+    render(home);
+});
+
+
+document.querySelector('#nav li:nth-child(2) > a').addEventListener('click', (e) => {
+    console.log(e.preventDefault());
+    console.log(e.target.href);
+    render(blog);
+});
+
+
+document.querySelector('#nav li:nth-child(3) > a').addEventListener('click', (e) => {
+    console.log(e.preventDefault());
+    console.log(e.target.href);
+    render(contact);
+});
+
+
+document.querySelector('#nav li:nth-child(4) > a').addEventListener('click', (e) => {
+    console.log(e.preventDefault());
+    console.log(e.target.href);
+    render(projects);
+});
+
 
 // nameChecker();
--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,6 @@
 export default function Content(){
     return `
 
-
 <div id="content">
 <h2>Poems</h2>
  <li>"The Playlist"</li>
@@ -11,5 +10,18 @@ export default function Content(){
     <hr>
 </hr>
 <h2>Visuals</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/CfYL1J7sAGs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     <li></li>
+ </iframe>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/A8ihEqDeHS0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     <li></li>
+ </iframe>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/kk_UsVItLWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     <li></li>
+ </iframe>
+</div>
+<button id="test">
+    Click Me!
+    </button>
 `;
 }
